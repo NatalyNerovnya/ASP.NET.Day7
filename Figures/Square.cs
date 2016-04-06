37965d70c178790255f64d369fadf05fa9c0973a
Наташа Неровня <natanerovnja@gmail.com>
2016-04-06 17:06:22 +0300

    create square class

---
+++ Figures/Square.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Figures
+{
+    public class Square: Rectangle,IFigure
+    {
+        public Square() : base() { }
+
+        public Square(double a) : base(a, a)
+        { }
+    }
+
+}

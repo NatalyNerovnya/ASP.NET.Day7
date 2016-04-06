d02c41f54040f1adec6e5db0573a7e996ee3da9f
Наташа Неровня <natanerovnja@gmail.com>
2016-04-06 14:38:42 +0300

    Class describing geometric figures

---
+++ Figures/Rectangle.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Permissions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Figures
+{
+    public class Rectangle
+    {
+        private double sideA, sideB;
+
+        public double SideA 
+        {
+            get { return sideA; }
+            set
+            {
+                if(value < 0)
+                    throw new ArgumentException();
+                sideA = value;
+            }
+        }
+
+        public Rectangle()
+        {
+
+        }
+    }
+}

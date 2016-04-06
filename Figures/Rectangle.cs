18b2bf56dab11e07c29b6ca9237a54d2d16f96f2
Наташа Неровня <natanerovnja@gmail.com>
2016-04-06 15:06:24 +0300

    class discribed rectangle

--- Figures/Rectangle.cs
+++ Figures/Rectangle.cs
@@ -7,14 +7,17 @@ using System.Threading.Tasks;
 
 namespace Figures
 {
-    public class Rectangle
+    public class Rectangle :IFigure, IEquatable<Rectangle>
     {
+        #region Fields
         private double sideA, sideB;
+        #endregion
 
+        #region Properties
         public double SideA 
         {
             get { return sideA; }
-            set
+            private set
             {
                 if(value < 0)
                     throw new ArgumentException();
@@ -22,9 +25,65 @@ namespace Figures
             }
         }
 
+        public double SideB
+        {
+            get { return sideB; }
+            private set
+            {
+                if (value < 0)
+                    throw new ArgumentException();
+                sideB = value;
+            }
+        }
+        #endregion
+
+        #region Constructors
         public Rectangle()
         {
-
+            SideA = 1;
+            SideB = 1;
         }
+
+        public Rectangle(double a, double b)
+        {
+            if(a == Double.NaN || b == Double.NaN ||
+                a == Double.NegativeInfinity || b == Double.NegativeInfinity ||
+                a == Double.PositiveInfinity || b == Double.PositiveInfinity)
+                throw new ArgumentException();
+
+            SideA = a;
+            SideB = b;
+        }
+
+        #endregion
+
+        #region Public Methods
+
+        public  double  Area()
+        {
+            return SideA*SideB;
+        }
+
+        public double Perimeter()
+        {
+            return 2*(SideA + SideB);
+        }
+
+        public double GetDiagonal()
+        {
+            return Math.Pow(SideA*SideA + SideB*SideB, 0.5);
+        }
+
+        public double GetRadiusCircumcircle()
+        {
+            return (double)GetDiagonal()/2;
+        }
+
+        public bool Equals(Rectangle other)
+        {
+            return other.SideA.Equals(SideA) && other.SideB.Equals(SideB) ? true : false;
+        }
+
+        #endregion
     }
 }
